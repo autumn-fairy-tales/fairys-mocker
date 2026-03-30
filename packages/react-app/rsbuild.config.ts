607417808a0e036d3f484c292e288b2904eb0914
SunLxy <1011771396@qq.com>
2026-03-30 15:03:15 +0800

    fix: 修改数据

--- packages/react-app/rsbuild.config.ts
+++ packages/react-app/rsbuild.config.ts
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/postcss';
 
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
+  html: {
+    title: 'Fairys Mocker',
+  },
   plugins: [pluginReact()],
   tools: {
     postcss: {
